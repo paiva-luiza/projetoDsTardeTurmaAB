981cdede466194cc352dbc090b4967d3b5fa8ad1
MdevSs <leandrohenrique.denise@gmail.com>
2025-11-18 20:57:39 -0300

    Criando diretório e arquivo para criar backup do projeto backend

--- backend/backup/backup.py
+++
@@ -1,9 +0,0 @@
-from app.db.supabase_client import get_supabase
-
-sb = get_supabase()
-
-# [ ] - Pegar os dados de cada tabela e colocar num arquivo JSON
-# [ ] - Pegar os arquivos do backend e colocar numa pasta separada
-# [ ] - Zipar essa pasta com tudo 
-data = sb.table('users').select("*").execute()
-print(data.data)
