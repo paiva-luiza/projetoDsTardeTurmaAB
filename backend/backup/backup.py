c0868cb8a068f7984a7894955aabb1aca9c70d7b
MdevSs <leandrohenrique.denise@gmail.com>
2025-11-18 21:56:29 -0300

    Adicionando o backup.zip no .gitignore

--- backend/backup/backup.py
+++ backend/backup/backup.py
@@ -1,14 +1,14 @@
 import shutil
 import os
 
+# Pega o caminho do arquivo executado
 diretorio = os.path.abspath(__file__);
 
-print(diretorio)
-# if(diretorio.__contains__("\\backup")==True):
-#     diretorio = diretorio.replace("\\backup", "\\")
-#     print(diretorio)
+# Debbug com o caminho sem o arquivo e a pasta backup/ (backup/backup.py)
 print(diretorio[:-16])
+# Atribuição do no caminho
 diretorio = diretorio[:-16]
+# Criando arquivo .zip
 shutil.make_archive(
     base_name="backend_backup",
     format='zip',
