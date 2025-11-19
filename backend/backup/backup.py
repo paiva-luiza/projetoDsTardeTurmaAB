652d459a6e2b1d4bca7756523c1ff112238434f5
MdevSs <leandrohenrique.denise@gmail.com>
2025-11-18 21:44:15 -0300

    Otimizando busca por caminho de execução do arquivo - Backup com caminho dinâmico feita

--- backend/backup/backup.py
+++ backend/backup/backup.py
@@ -1,12 +1,14 @@
 import shutil
 import os
 
-diretorio = os.getcwd();
-diretorio += "\\backend\\"
+diretorio = os.path.abspath(__file__);
+
 print(diretorio)
-if(diretorio.__contains__("\\backup")==True):
-    diretorio = diretorio.replace("\\backup", "\\")
-    print(diretorio)
+# if(diretorio.__contains__("\\backup")==True):
+#     diretorio = diretorio.replace("\\backup", "\\")
+#     print(diretorio)
+print(diretorio[:-16])
+diretorio = diretorio[:-16]
 shutil.make_archive(
     base_name="backend_backup",
     format='zip',
