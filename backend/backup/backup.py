625c18030719a311897e6b84cb8c132af5beb187
MdevSs <leandrohenrique.denise@gmail.com>
2025-11-18 22:13:40 -0300

    Otimizando ainda mais o script de backup

--- backend/backup/backup.py
+++ backend/backup/backup.py
@@ -1,16 +1,16 @@
 import shutil
 import os
 
-# Pega o caminho do arquivo executado
-diretorio = os.path.abspath(__file__);
+# Caminho da pasta do script
+diretorio_script = os.path.dirname(os.path.abspath(__file__))
 
-# Debbug com o caminho sem o arquivo e a pasta backup/ (backup/backup.py)
-print(diretorio[:-16])
-# Atribuição do no caminho
-diretorio = diretorio[:-16]
-# Criando arquivo .zip
+# Caminho acima (backend/)
+diretorio_backend = os.path.dirname(diretorio_script)
+
+print("Diretório backend:", diretorio_backend)
+
 shutil.make_archive(
     base_name="backend_backup",
     format='zip',
-    root_dir=diretorio.replace("/", "\\")
+    root_dir=diretorio_backend.replace("/", "\\")
 )
